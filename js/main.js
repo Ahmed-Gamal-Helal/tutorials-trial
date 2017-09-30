6fd46529e1ab96d61e2d2be2c972451b14d23ee1
Ahmed Gamal <ahmdtawfiek@gmail.com>
2017-09-30 17:35:30 +0200

    fifth commit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope){
+app.controller('myCtrl', function($scope) {
 
     $scope.details = [];
     $scope.msg = '';
+
     // first function to add the input data
+    
     $scope.add = function () {
         if (!$scope.name || !$scope.phone || !$scope.email) {
             $scope.msg = "Can not be displayed";
@@ -15,13 +17,10 @@ app.controller('myCtrl', function($scope){
             };
             $scope.details.push(contact);
           }
-        }
-    // second function to edit input data 
-    $scope.edit = function () {
-
-    }
+        };
+    // second function to edit input data $scope.edit = function () {}
     //third function to delete unuseful data
-    $scope.delete = function (x) {
+    $scope.removeItem = function (x){
         $scope.details.splice(x, 1);
-    }
+    };
 });
